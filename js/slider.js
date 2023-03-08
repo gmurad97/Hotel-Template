2e3bde412b140d1259b50b7dae1e34b2095e8a94
gmurad97 <murad.dev@bk.ru>
2023-03-09 02:51:55 +0400

    Add header slider and bug fix

---
+++ js/slider.js
@@ -0,0 +1,38 @@
+let images = document.querySelector(".hs-images").getElementsByTagName("img");
+let imagePosition = 0;
+
+function deactiveImages() {
+    for (let image of images) {
+        if (image.classList.contains("hs-images-active")) {
+            image.classList.remove("hs-images-active");
+        }
+    }
+}
+
+function nextSlide() {
+    imagePosition++;
+    if (imagePosition >= images.length) {
+        imagePosition = 0;
+    }
+    deactiveImages();
+    images[imagePosition].classList.add("hs-images-active");
+}
+
+function prevSlide() {
+    imagePosition--;
+    if (imagePosition < 0) {
+        imagePosition = images.length - 1;
+    }
+    deactiveImages();
+    images[imagePosition].classList.add("hs-images-active");
+}
+
+let autoPlaySlider = setInterval(nextSlide, 2048);
+
+document.getElementsByClassName("hs-images")[0].onmouseenter = function () {
+    clearInterval(autoPlaySlider);
+}
+
+document.getElementsByClassName("hs-images")[0].onmouseleave = function () {
+    autoPlaySlider = setInterval(nextSlide, 2048);
+}
